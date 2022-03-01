209b73fe85677f84f1a09bccab2f0142ef1286f0
Grigory Vasilev <grishavasilev62@gmail.com>
2022-03-02 02:28:12 +0300

    Password Generator

--- InformationProtectSolution/LR1.Lib/PasswordGenerator.cs
+++ InformationProtectSolution/LR1.Lib/PasswordGenerator.cs
@@ -2,7 +2,23 @@
 
 namespace LR1.Lib
 {
+	/// <summary>
+	/// Generate Password.
+	/// </summary>
 	class PasswordGenerator
 	{
+		private Random randomGenerator = new Random();
+
+		public string GeneratedPassword;
+
+		public PasswordGenerator(string SymbolsCollection, int size)
+		{
+			for (int symbolIndex = 0; symbolIndex < size; symbolIndex++)
+			{
+				var symbolPos = randomGenerator.Next(SymbolsCollection.Length);
+
+				GeneratedPassword += SymbolsCollection[symbolPos];
+			}
+		}
 	}
 }
